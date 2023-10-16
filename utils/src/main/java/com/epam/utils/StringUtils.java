b0ffdf89819d43252443488814c97dab33720cd2
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-16 17:04:03 +0300

    Completed task

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,19 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
-    }
+package com.epam.utils;
+
+        public class StringUtils {
+            public static boolean isPositiveNumber(String str) {
+                if (str == null){
+                    return false;
+                }else if (str.contains("-") || str.contains("null") || str.equals("") || str.contains(" ")){
+                    return false;
+                }else if (str.contains(".")){
+                    return Double.parseDouble(str) > 0;
+                }else {
+                    return Integer.parseInt(str) > 0;
+                }
+            }
+        }    }
 }
