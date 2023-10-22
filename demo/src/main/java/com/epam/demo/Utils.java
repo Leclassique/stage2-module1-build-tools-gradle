dd7e96da942554b9dfe0deb5ee5bb3ec22380fda
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-22 23:29:51 +0300

    Fixed task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,12 +1,13 @@
 package com.epam.demo;
 
-import com.epam.utils.StringUtils;
-
 import java.util.List;
 
+import static com.epam.utils.StringUtils.isPositiveNumber;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream()
-                .allMatch(StringUtils::isPositiveNumber);
+        for (String arg: args) {
+            if (!isPositiveNumber(arg)) return false;
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,20 +1,8 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-package com.epam.utils;
-
-        public class StringUtils {
-            public static boolean isPositiveNumber(String str) {
-                if (str == null){
-                    return false;
-                }else if (str.contains("-") || str.contains("null") || str.equals("") || str.contains(" ")){
-                    return false;
-                }else if (str.contains(".")){
-                    return Double.parseDouble(str) > 0;
-                }else {
-                    return Integer.parseInt(str) > 0;
-                }
-            }
-        }    }
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
+    }
 }
